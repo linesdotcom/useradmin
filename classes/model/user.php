0b391bdde37b57d8433554730864be41f3abd627
Jonathan Pike <code@staite.net>
2013-05-15 19:39:48 +0000

    added _table_columns for those models missing it

--- classes/model/user.php
+++ classes/model/user.php
@@ -1,3 +1,21 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
 
-class Model_User extends Useradmin_Model_User { }
+class Model_User extends Useradmin_Model_User {
+
+	protected $_table_columns = array(
+		'id' 						=> array('type' => 'int'),
+		'email' 					=> array('type' => 'string'),
+		'username' 					=> array('type' => 'string'),
+		'password' 					=> array('type' => 'string'),
+		'password_updated' 			=> array('type' => 'string'),
+		'logins' 					=> array('type' => 'int'),
+		'last_login' 				=> array('type' => 'int'),
+		'status' 					=> array('type' => 'string'),
+		'failed_login_count' 		=> array('type' => 'int'),
+		'created' 					=> array('type' => 'string'),
+		'modified' 					=> array('type' => 'string'),
+		'last_failed_login' 		=> array('type' => 'string'),
+		'authorizenet_profile_id' 	=> array('type' => 'int'),
+	);
+
+}
--- classes/model/user/identity.php
+++ classes/model/user/identity.php
@@ -1,3 +1,12 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
 
-class Model_User_Identity extends Useradmin_Model_User_Identity { }
+class Model_User_Identity extends Useradmin_Model_User_Identity {
+
+	protected $_table_columns = array(
+		'id' 		=> array('type' => 'int'),
+		'user_id' 	=> array('type' => 'int'),
+		'provider' 	=> array('type' => 'string'),
+		'identity' 	=> array('type' => 'string'),
+	);
+
+}
